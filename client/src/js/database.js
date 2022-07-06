210179a078e450e2da0c93fad776db0600bac5e6
Sheri Brown <sherib70@gmail.com>
2022-07-06 18:36:20 -0400

    fixed database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -38,7 +38,7 @@ export const getDb = async () => {
     const request = store.get(1);
     const result = await request;
     console.log("result.value", result);
-    return result;
+    return result.value
   } catch (err) {
     console.error("getDb not implemented");
   }
