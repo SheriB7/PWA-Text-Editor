9b0192feac8196e6ea0a1bff4183203a0e51af03
Sheri Brown <sherib70@gmail.com>
2022-07-06 18:14:44 -0400

    data changes

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,47 +1,32 @@
-const { warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
-const { registerRoute } = require("workbox-routing");
-const { CacheableResponsePlugin } = require("workbox-cacheable-response");
-const { ExpirationPlugin } = require("workbox-expiration");
-const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
+import { registerRoute } from "workbox-routing";
+import { StaleWhileRevalidate, CacheFirst } from "workbox-strategies";
+import { precacheAndRoute } from "workbox-precaching";
+import { CacheableResponsePlugin } from "workbox-cacheable-response";
+
+// Import the expiration plugin
+import { ExpirationPlugin } from "workbox-expiration";
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-const pageCache = new CacheFirst({
-  cacheName: "page-cache",
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
-
-warmStrategyCache({
-  urls: ["/index.html", "/"],
-  strategy: pageCache,
-});
-
-registerRoute(({ request }) => request.mode === "navigate", pageCache);
-
-registerRoute(({ request }) => {
+const cacheName = "static-resources";
+const matchCallback = ({ request }) => {
+  console.log(request);
   return (
     // CSS
     request.destination === "style" ||
     // JavaScript
-    request.destination === "script" ||
-    request.destination === "worker" 
-  ),
-  // console.log(request);
+    request.destination === "script"
+  );
+};
+
+registerRoute(
+  matchCallback,
   new StaleWhileRevalidate({
-    cacheName: "assetCache",
+    cacheName,
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
     ],
-  });
-  
-});
+  })
+);
